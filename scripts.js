ee4ec97dee417b1b5df6bf7477e0f025e21e81b4
patrickadventure <patrickdhital14@gmail.com>
2024-07-14 10:39:10 +1000

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -1,44 +1,47 @@
-document.addEventListener('DOMContentLoaded', function () {
-  loadHeaderFooter();
-
-  // Dropdown functionality for mobile view
-  const dropdown = document.querySelector('.dropdown > a');
-  if (dropdown) {
-    dropdown.addEventListener('click', function (e) {
-      e.preventDefault();
-      this.parentElement.classList.toggle('active');
-    });
-  }
+document.addEventListener('DOMContentLoaded', function() {
+    loadHeaderFooter();
 });
 
 function loadHeaderFooter() {
-  fetch('header.html')
-    .then(response => response.text())
-    .then(data => {
-      document.body.insertAdjacentHTML('afterbegin', data);
-    });
+    fetch('header.html')
+        .then(response => response.text())
+        .then(data => {
+            document.body.insertAdjacentHTML('afterbegin', data);
+            setupDropdown(); // Setup dropdown after the header is loaded
+        });
 
-  fetch('footer.html')
-    .then(response => response.text())
-    .then(data => {
-      document.body.insertAdjacentHTML('beforeend', data);
+    fetch('footer.html')
+        .then(response => response.text())
+        .then(data => {
+            document.body.insertAdjacentHTML('beforeend', data);
+        });
+}
+
+function setupDropdown() {
+    // Dropdown functionality for mobile view
+    const dropdownToggles = document.querySelectorAll('.dropdown > a');
+    dropdownToggles.forEach(dropdown => {
+        dropdown.addEventListener('click', function(e) {
+            e.preventDefault();
+            this.parentElement.classList.toggle('active');
+        });
     });
 }
 
 function showDrunkModePopup() {
-  const popup = document.getElementById('drunkModePopup');
-  popup.classList.remove('hidden');
-  document.body.classList.add('overlay');
+    const popup = document.getElementById('drunkModePopup');
+    popup.classList.remove('hidden');
+    document.body.classList.add('overlay');
 }
 
 function unlockDrunkMode() {
-  const popup = document.getElementById('drunkModePopup');
-  popup.classList.add('hidden');
-  document.body.classList.remove('overlay');
-  const buttonContainer = document.querySelector('.profile-view .stats button');
-  buttonContainer.style.display = 'block';
+    const popup = document.getElementById('drunkModePopup');
+    popup.classList.add('hidden');
+    document.body.classList.remove('overlay');
+    const buttonContainer = document.querySelector('.profile-view .stats button');
+    buttonContainer.style.display = 'block';
 }
 
 function goBack() {
-  window.history.back();
+    window.history.back();
 }
