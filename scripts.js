c81475aed96b4dbf5d2cd8c34d4ecc05ea6c8e97
patrickadventure <patrickdhital14@gmail.com>
2024-07-28 18:14:11 +1000

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -3,18 +3,22 @@ document.addEventListener('DOMContentLoaded', function() {
 });
 
 function loadHeaderFooter() {
-    fetch('header.html')
-        .then(response => response.text())
-        .then(data => {
-            document.body.insertAdjacentHTML('afterbegin', data);
-            setupDropdown(); // Setup dropdown after the header is loaded
-        });
+    if (!document.getElementById('main-header')) {
+        fetch('header.html')
+            .then(response => response.text())
+            .then(data => {
+                document.body.insertAdjacentHTML('afterbegin', data);
+                setupDropdown(); // Setup dropdown after the header is loaded
+            });
+    }
 
-    fetch('footer.html')
-        .then(response => response.text())
-        .then(data => {
-            document.body.insertAdjacentHTML('beforeend', data);
-        });
+    if (!document.getElementById('main-footer')) {
+        fetch('footer.html')
+            .then(response => response.text())
+            .then(data => {
+                document.body.insertAdjacentHTML('beforeend', data);
+            });
+    }
 }
 
 function setupDropdown() {
@@ -26,22 +30,4 @@ function setupDropdown() {
             this.parentElement.classList.toggle('active');
         });
     });
-}
-
-function showDrunkModePopup() {
-    const popup = document.getElementById('drunkModePopup');
-    popup.classList.remove('hidden');
-    document.body.classList.add('overlay');
-}
-
-function unlockDrunkMode() {
-    const popup = document.getElementById('drunkModePopup');
-    popup.classList.add('hidden');
-    document.body.classList.remove('overlay');
-    const buttonContainer = document.querySelector('.profile-view .stats button');
-    buttonContainer.style.display = 'block';
-}
-
-function goBack() {
-    window.history.back();
 }
